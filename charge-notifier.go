5d50b1dab5c91116e53071276e8e8025b0f414e0
Your Name <you@example.com>
2021-02-18 17:23:08 +0100

    Add automatic system suspension (systemd specific)

--- charge-notifier.go
+++ charge-notifier.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"os/exec"
 	"time"
 
 	"github.com/0xAX/notificator"
@@ -16,6 +17,7 @@ func main() {
 	//Thresholds
 	tHigh := 0.8
 	tLow := 0.2
+	tOff := 0.05
 
 	sleepTime := 180.0
 
@@ -42,12 +44,22 @@ func main() {
 
 			if tLow < battCharge && battCharge < tHigh {
 				fmt.Println("We're good. Battery is between", tLow, "and", tHigh)
-			} else if tLow > battCharge && state == 4 { //Wi Tu Lo
+			} else if tLow > battCharge && state == 4 { // Wi Tu Lo
 				fmt.Println("Too low and discharging!")
-				notify.Push("Battery low", "Start charging to preserve battery healh", "battery-low", notificator.UR_NORMAL)
-
-				st *= battCharge / tLow
-			} else if tHigh < battCharge && state == 3 { //Bang Ding Ow
+				if tOff > battCharge && state == 4 { // Bang Ding Ow
+					notify.Push("Charge level critical", "Suspending", "battery-critical", notificator.UR_NORMAL)
+					fmt.Println("Suspending")
+					time.Sleep(3 * time.Second)
+					exec.Command("systemctl", "suspend").Run() // Linux/Systemd specific. echo "mem" > /sys/power/state would require root privileges
+					time.Sleep(5 * time.Second)
+					fmt.Println("Woke back up")
+					notify.Push("Charge level was too low", "Your system was suspended", "battery-low", notificator.UR_NORMAL)
+					st = 7
+				} else {
+					notify.Push("Battery low", "Start charging to preserve battery healh", "battery-low", notificator.UR_NORMAL)
+					st *= battCharge / tLow
+				}
+			} else if tHigh < battCharge && state == 3 { // Sum Ting Wong
 				fmt.Println("Too high and charging!")
 				notify.Push("Battery high", "Stop charging to preserve battery healh", "battery-high", notificator.UR_NORMAL)
 				st *= battCharge / tHigh
